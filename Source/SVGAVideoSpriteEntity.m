75377de0bfa3716b459c8711bd2df4ec3feb4d15
一笔一划.py <1805004786@qq.com>
2025-10-08 23:16:00 -0700

    Update SVGAVideoSpriteEntity.m

--- Source/SVGAVideoSpriteEntity.m
+++ Source/SVGAVideoSpriteEntity.m
@@ -66,6 +66,7 @@
     if (bitmap != nil) {
         layer.bitmapLayer = [[SVGABitmapLayer alloc] initWithFrames:self.frames];
         layer.bitmapLayer.contents = (__bridge id _Nullable)([bitmap CGImage]);
+        layer.bitmapLayer.contentsGravity = kCAGravityResizeAspectFill;
     }
     layer.vectorLayer = [[SVGAVectorLayer alloc] initWithFrames:self.frames];
     return layer;
